be88eea74b2d845e041c930423eb2edb85bc07cf
glikched <florianvictorcristea@gmail.com>
2024-02-15 21:54:53 +0200

    r9q: commonize recovery for all r9q versions

--- twrp_r9q.mk
+++ twrp_r9q.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/r9q/recover
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := twrp_r9q
 PRODUCT_DEVICE := r9q
-PRODUCT_MODEL := SM-G990U
+PRODUCT_MODEL := SM-G990X
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
